5c1be84439c6246090839fe5cbd348486a32411e
akorzh <alexey.korzh.kas@gmail.com>
2021-10-09 16:22:09 +0600

    WEBPRO-11:Добавление слова api в URL для всех запросов

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/";
+const API_URL = "http://localhost:8080/api/";
 
 class AuthService {
 
@@ -14,7 +14,7 @@ class AuthService {
 
     login(login, password) {
         return axios
-            .post(API_URL + "api/login", {
+            .post(API_URL + "login", {
                 login,
                 password
             })
--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/';
+const API_URL = 'http://localhost:8080/api/';
 
 class MovieService {
     getMovie(id) {
